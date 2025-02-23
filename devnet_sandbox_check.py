0a0bb7fdb575ae9e5a459122b4a5f45cc55c43b5
Alex Perkins <alex.perkins@protonmail.com>
2025-02-22 19:48:15 -0500

    added timestamp, os checker, and made it pretty

--- devnet_sandbox_check.py
+++ devnet_sandbox_check.py
@@ -1,23 +1,28 @@
 import os
+import platform
+from datetime import datetime
 
 print("Which DevNet Sandbox always-on resources are reachable?")
 
 # list of devices that have a public IP address
-devices = {"ACI": "https://sandboxapicdc.cisco.com",
-		   "Catalyst Center": "https://sandboxdnac.cisco.com",
-		   "ISE": "https://devnetsandboxise.cisco.com",
-		   "IOS XE": "https://devnetsandboxiosxe.cisco.com",
-		   "IOS XR": "https://sandboxiosxr-1.cisco.com",
-		   "NSO": "https://sandbox-nso-1.cisco.com",
-		   "NX-OS": "https://sbx-nxos-mgmt.cisco.com",
-		   "SD-WAN": "https://sandbox-sdwan-2.cisco.com"
-		   }
+devices = {
+	"ACI": "sandboxapicdc.cisco.com",
+	"Catalyst Center": "sandboxdnac.cisco.com",
+	"ISE": "devnetsandboxise.cisco.com",
+	"IOS XE": "devnetsandboxiosxe.cisco.com",
+	"IOS XR": "sandboxiosxr-1.cisco.com",
+	"NSO": "sandbox-nso-1.cisco.com",
+	"NX-OS": "sbx-nxos-mgmt.cisco.com",
+	"SD-WAN": "sandbox-sdwan-2.cisco.com"
+}
+
+# adjust ping command depending on OS
+ping_cmd = "ping -c 4 " if platform.system() != "Windows" else "ping -n 4 "
+
+# add timestamp
+now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 # loop over each entry in the devices dictionary and ping the device to see if it is reachable
 for device, address in devices.items():
-	response = os.system("ping -c 4 " + address)
-	if response == 0:
-		print(f"{device} is reachable")
-	else:
-		print(f"{device} is not reachable")
-
+	response = os.system(ping_cmd + address)
+	print(f"{device:<15} : {'reachable' if response == 0 else 'not reachable'} at {now}")
