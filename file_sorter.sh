e59bd440a46f327cddbc96eebd15bf5a0e5906cc
bumpsinthewire <bumpsinthewire@gmail.com>
2025-03-16 21:25:56 -0400

    got rid of the for loop in the file_sorter

--- file_sorter.sh
+++ file_sorter.sh
@@ -8,8 +8,4 @@ touch /home/$USER/move_log.txt
 mkdir -p $DEST_DIR
 
 # Find all files in the source directory that are larger than 10MB and move them to a new directory called big_files in the user's home directory
-for file in $(find "$SOURCE_DIR" -size +10M); do
-	mv "$file" $DEST_DIR
-	echo "Moved '$file' to $DEST_DIR" >> /home/$USER/move_log.txt
-done
-
+find "$SOURCE_DIR" -size +10M -exec sh -c 'mv "$1" "$2" && echo "Moved \"$1\" to \"$2\"" >> /home/$USER/move_log.txt' _ "{}" "$DEST_DIR" \;
